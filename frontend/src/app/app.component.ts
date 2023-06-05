e7c346822f8b6f7070e7a4f43b6f08a989e5b0c4
jack <brjat@uw.edu>
2023-06-05 16:02:07 -0700

    Create chart and provide user with year/species mock options

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {WhaleSighting} from './whale_sightings/whale_sighting.model';
-import { WhaleSightingsApiService } from './whale_sightings/whale_sightings-api.service';
-import { Subscription } from 'rxjs';
+import { WhaleSightingsApiService } from './whale-sightings/whale-sightings-api.service';
+import { ChartDataset, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +9,47 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
-  whaleSightingsList: any;
-
-  constructor(private whaleSightingsApi: WhaleSightingsApiService) {
-  }
-
-  ngOnInit() {
-    this.whaleSightingsApi
-      .getWhaleSightings()
-      .subscribe(res => {
-          this.whaleSightingsList = res;
+  // Initialize selectedYear and selectedSpecies
+  selectedYear: number | null = null;
+  selectedSpecies: string | null = null;
+  chartData: ChartDataset[] = [];
+  chartLabels: any[] = [];
+  chartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 1,
+          precision: 0
         }
-      );
-  }
+      }
+    }
+  };
+  chartLegend = true;
+  showGraph = false;
 
-  ngOnDestroy() {
+  constructor(private whaleSightingsApi: WhaleSightingsApiService) {}
 
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  onSelectionChange() {
+    if (this.selectedYear && this.selectedSpecies) {
+      this.whaleSightingsApi.getWhaleSightings(this.selectedYear, this.selectedSpecies)
+        .subscribe(res => {
+          this.showGraph = true;
+          const data = res as any[];
+          console.log(data);
+          const observationCounts = data.map(item => item.observationcount);
+          const months = ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'];
+          this.chartData = [{ data: observationCounts, label: 'Observation Count' }];
+          this.chartLabels = months;
+        });
+    } else {
+      this.showGraph = false;
+    }
   }
 }
